7cbf4db3ddb2fd5387152cd0823cee755da6a7fe
Lucas Martins <lucas.martins.fsa@gmail.com>
2024-01-22 23:36:16 -0300

    refatora

--- src/main/java/com/devlucasmartins/clima/controller/PrevisaoTempoController.java
+++ src/main/java/com/devlucasmartins/clima/controller/PrevisaoTempoController.java
@@ -5,11 +5,9 @@ import com.devlucasmartins.clima.service.HGBrasilService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("weather")
 @RequiredArgsConstructor
 public class PrevisaoTempoController {
     private final HGBrasilService service;
--- src/main/java/com/devlucasmartins/clima/service/HGBrasilService.java
+++ src/main/java/com/devlucasmartins/clima/service/HGBrasilService.java
@@ -2,15 +2,18 @@ package com.devlucasmartins.clima.service;
 
 import com.devlucasmartins.clima.dto.HGBrasilResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class HGBrasilService {
     private final HGBrasilClientService client;
+    @Value("${hgbrazil.apiKey}")
+    private String apiKey;
 
     public HGBrasilResponse fetchWeatherForCity(String city) {
-        String apiKey = "aa5bcb40";
+
         int arrayLimit = 3;
         String fields = "only_results,temp,city_name,forecast,max,min,date";
 
